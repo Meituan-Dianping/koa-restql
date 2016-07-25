6ce98d0536f3cf1bc30d6bd9a118f3d49497602d
zhangchi07 <zhangchi07@meituan.com>
2016-07-25 15:57:56 +0800

    Added: pagination test

--- test/mock/models/user.js
+++ test/mock/models/user.js
@@ -10,13 +10,13 @@ module.exports.attributes = (DataTypes) => {
     },   
 
     name: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(100),
       allowNull: false,
       defaultValue: ''
     }, 
 
     nickname: {
-      type: DataTypes.STRING,
+      type: DataTypes.STRING(100),
       allowNull: false,
       defaultValue: ''
     }, 
